1238a095e6da4601f2253a2bd2bbe1a4e3a3ca9b
zhaojunhui <zjh@com>
2017-11-07 20:04:29 +0800

    登陆退出完成

--- public/js/common.js
+++ public/js/common.js
@@ -1,8 +1,32 @@
-
-	NProgress.start();
-
-	NProgress.done();
-
+define(['jquery','template','cookie'],function($,template){
+	// 侧边栏点击效果
 	$('.navs ul').prev('a').on('click', function () {
 		$(this).next().slideToggle();
 	});
+// 退出登录
+	 $('#btnOut').click(function(){
+		 
+		$.ajax({
+			type:'post',
+			url:'/api/logout',
+			dataType:'json',
+			success:function(data){
+				if(data.code == 200){
+					location.href = "/"
+				}
+			}
+		})
+    })
+
+	// 获取登陆信息
+	var info = $.cookie('loginInfo');
+	// console.log(info)
+	var tpl =  '<div class="avatar img-circle">'
+                +'<img src="{{tc_avatar}}">'
+           		+'</div>'
+           		+'<h4>{{tc_name}}</h4>';
+	var html = template.render(tpl,JSON.parse(info));
+	// console.log(html);
+	$('.aside .profile').html(html);
+})
+	
---
+++ public/js/login.js
@@ -0,0 +1,20 @@
+define(['jquery','cookie'], function($) {
+        $("#btn").click(function(){
+            
+            $.ajax({
+                type:'post',
+                url:'/api/login',
+                data:$("#FormData").serialize(),
+                dataType:'json',
+                success:function(data){
+                    // console.log(data)
+                    if(data.code == 200){
+                        $.cookie('loginInfo',JSON.stringify(data.result),{path:'/'});
+                        location.href = "index.php/index/index";
+                    }
+                }
+            })
+            return false;
+        })
+    
+});
