af05b86b678e3a300a2c554f4177cbc8db448fa5
zhaojunhui <zjh@com>
2017-11-08 16:55:58 +0800

    实现讲师预览

--- public/js/teacher-list.js
+++ public/js/teacher-list.js
@@ -1,6 +1,7 @@
 define([
     'jquery',
-    'template'
+    'template',
+    'bootstrap'
 ], function($, template) {
     // 讲师管理
     $.ajax({
@@ -8,10 +9,28 @@ define([
         url : '/api/teacher',
         dataType : 'json',
         success : function(data){
-            console.log(data)
+            // console.log(data)
             var html = template('teacherTpl',{list:data.result});
             // console.log(html)
             $('#teacherInfo').html(html);
+            // 讲师查看
+            $('#teacherInfo').find('.examine').click(function(){
+                var td = $(this).parent();
+                var tcId = td.attr('data-tcId');
+                // console.log(tcId)
+                $.ajax({
+                    data : 'get',
+                    url : '/api/teacher/view',
+                    data : {tc_id : tcId},
+                    dataType : 'json',
+                    success : function(data){
+                        // console.log(data)
+                        var html = template('modalTpl',data.result);
+                        $('#modatInfo').html(html);
+                        $('#teacherModal').modal();
+                    }
+                })
+            })
         }
     })
     
