c97f989d3a574d953841e07c5e443a9d03d0d094
zhaojunhui <zjh@com>
2017-11-09 17:18:04 +0800

    完成注销和启用

--- public/js/teacher-list.js
+++ public/js/teacher-list.js
@@ -13,6 +13,32 @@ define([
             var html = template('teacherTpl',{list:data.result});
             // console.log(html)
             $('#teacherInfo').html(html);
+
+            // 启用和注销
+            $('#teacherInfo').find('.switchBtn').click(function(){
+                
+                var td = $(this).parent();
+                var tcId = td.attr('data-tcId');
+                var tcStatus = td.attr('data-tcstatus');
+                // alert(tcStatus)
+                var that = this;
+                $.ajax({
+                    data : 'post',
+                    url : '/api//teacher/handle',
+                    dataType : 'json',
+                    data : {tc_id : tcId,tc_status : tcStatus},
+                    success : function(data){
+                        td.attr('data-tcstatus',data.result.tc_status);
+                        // console.log(data)
+                        if(data.result.tc_status == 1){
+                            $(that).text('启 用');
+                        }else{
+                            $(that).text('注 销');
+                        }
+                    }
+                })
+            })
+
             // 讲师查看
             $('#teacherInfo').find('.examine').click(function(){
                 var td = $(this).parent();
